651617914a2dea1bfdf253ae1add85ecee866c92
dayat <dayatsmkn26@gmail.com>
2024-09-13 21:28:20 +0700

    update live url

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from 'react'
 import sendIcon from '/send.svg'
 import './App.css'
 
-const ws = new WebSocket("ws://localhost:3000/cable");
+const ws = new WebSocket("ws://ruby-on-rails-chat-api.onrender.com/cable");
 
 interface Message {
   id: number;
